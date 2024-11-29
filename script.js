54e6a582f223ebf7b9d194cb0d0d36d71089dda2
Gab102323 <gamboadavidgabriel@gmail.com>
2024-11-29 23:53:44 +0800

    changes in cleanser

--- script.js
+++ script.js
@@ -21,4 +21,26 @@ num = images.length-1;
 }
 slider.src = images[num];
 }
+// Array of image URLs
+const images = [
+    "cleanser.jpg",
+    "cleanser1.jpg",
+    "cleanser2.jpg",
+    "cleanser3.jpg",
+    "cleanser4.jpg",
+    "cleanser5.jpg"
+];
 
+let currentIndex = 0;
+
+// Reference the image element
+const slider = document.getElementById("slider");
+
+// Function to show the next image
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % images.length; // Loop back to the first image if at the end
+    slider.src = images[currentIndex];
+}
+
+// Add event listener to the image for swapping
+slider.addEventListener("click", showNextImage);
